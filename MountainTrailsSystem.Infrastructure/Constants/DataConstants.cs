b1d4e72a8d11d4283ef20a366e9095c117481447
Georgi Ivanov <givanov900@gmail.com>
2024-03-25 19:59:51 +0200

    Peak entity updated

--- MountainTrailsSystem.Infrastructure/Constants/DataConstants.cs
+++ MountainTrailsSystem.Infrastructure/Constants/DataConstants.cs
@@ -9,5 +9,15 @@
         //Region
         public const int RegionNameMaximumLength = 30;
         public const int RegionNameMinimumLength = 3;
+
+        //Peak
+        public const int PeakNameMaximumLength = 50;
+        public const int PeakNameMinimumLength = 3;
+
+        public const int PeakDescriptionMaximumLength = 500;
+        public const int PeakDescriptionMinimumLength = 30;
+
+        public const int PeakMaximumElevation = 8848;
+        public const int PeakMinimumElevation = 1;
     }
 }
--- MountainTrailsSystem.Infrastructure/Data/Models/Peak.cs
+++ MountainTrailsSystem.Infrastructure/Data/Models/Peak.cs
@@ -1,6 +1,43 @@
-﻿namespace MountainTrailsSystem.Infrastructure.Data.Models
+﻿using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using static MountainTrailsSystem.Infrastructure.Constants.DataConstants;
+
+namespace MountainTrailsSystem.Infrastructure.Data.Models
 {
+    [Comment("Mountain peak")]
     public class Peak
     {
+        [Key]
+        [Comment("Peak identifier")]
+        public int PeakId { get; set; }
+
+        [Required]
+        [MaxLength(PeakNameMaximumLength)]
+        [Comment("Peak name")]
+        public string Name { get; set; } = String.Empty;
+
+        [Required]
+        [MaxLength(PeakDescriptionMaximumLength)]
+        [Comment("Peak description")]
+        public string Description { get; set; } = String.Empty;
+
+        [Required]
+        [Range(PeakMinimumElevation, PeakMaximumElevation)]
+        [Comment("Peak elevation")]
+        public int Elevation { get; set; }
+        
+        [Required]
+        [Comment("Image URL of the peak")]
+        public string ImageUrl { get; set; } = String.Empty;
+
+        [Required]
+        [Comment("Mountain identifier")]
+        public int MountainId { get; set; }
+
+        [ForeignKey(nameof(MountainId))]
+        public Mountain Mountain { get; set; } = null!;
+
+        public ICollection<Trail> Trails { get; set; } = new List<Trail>();
     }
 }
