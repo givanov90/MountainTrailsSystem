93fc78e3653d061ca257f1615084ea1c454c7348
Georgi Ivanov <givanov900@gmail.com>
2024-03-25 21:01:46 +0200

    Enumeration for trail difficulty level added

---
+++ MountainTrailsSystem.Infrastructure/Enumerations/DifficultyLevel.cs
@@ -0,0 +1,12 @@
+﻿namespace MountainTrailsSystem.Infrastructure.Enumerations
+{
+    public enum DifficultyLevel
+    {
+        Easy = 0,
+        Moderate = 1,
+        Intermediate = 2,
+        Difficult = 3,
+        VeryDifficult = 4,
+        Extreme = 5,
+    }
+}
