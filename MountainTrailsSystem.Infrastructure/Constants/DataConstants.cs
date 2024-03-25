986e99692ff200c79fb53295f405222f2f53f764
Georgi Ivanov <givanov900@gmail.com>
2024-03-25 19:16:14 +0200

    DataConstants class added

---
+++ MountainTrailsSystem.Infrastructure/Constants/DataConstants.cs
@@ -0,0 +1,9 @@
+﻿namespace MountainTrailsSystem.Infrastructure.Constants
+{
+    public static class DataConstants
+    {
+        //Mountain
+        public const int MountainNameMaximumLength = 50;
+        public const int MountainNameMinimumLength = 3;
+    }
+}
