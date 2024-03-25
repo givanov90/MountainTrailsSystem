e1f87e8ab0f63aea81c62aec621a671fe34d0b4b
Georgi Ivanov <givanov900@gmail.com>
2024-03-25 21:32:14 +0200

    TrailPeak navigation entity updated

--- MountainTrailsSystem.Infrastructure/Data/Models/TrailPeak.cs
+++ MountainTrailsSystem.Infrastructure/Data/Models/TrailPeak.cs
@@ -1,6 +1,24 @@
-﻿namespace MountainTrailsSystem.Infrastructure.Data.Models
+﻿using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace MountainTrailsSystem.Infrastructure.Data.Models
 {
+    [Comment("Connection between trail and peak entities")]
     public class TrailPeak
     {
+        [Key]
+        [Comment("Trail identifier")]
+        public int TrailId { get; set; }
+
+        [ForeignKey(nameof(TrailId))]
+        public Trail Trail { get; set; } = null!;
+
+        [Key]
+        [Comment("Peak identifier")]
+        public int PeakId { get; set; }
+
+        [ForeignKey(nameof(PeakId))]
+        public Peak Peak { get; set; } = null!;
     }
 }
