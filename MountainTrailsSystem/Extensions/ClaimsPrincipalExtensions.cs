835f5c05dac873c83432a187c2e07e8b5abaede6
Georgi Ivanov <givanov900@gmail.com>
2024-04-04 18:59:40 +0300

    ClaimsPrincipalExtensions class with IsAdministrator method added

---
+++ MountainTrailsSystem/Extensions/ClaimsPrincipalExtensions.cs
@@ -0,0 +1,13 @@
+﻿using System.Security.Claims;
+using static MountainTrailsSystem.Core.Constants.RoleConstants;
+
+namespace MountainTrailsSystem.Extensions
+{
+    public static class ClaimsPrincipalExtensions
+    {
+        public static bool IsAdministrator(this ClaimsPrincipal user)
+        {
+            return user.IsInRole(AdministratorRole);
+        }
+    }
+}
