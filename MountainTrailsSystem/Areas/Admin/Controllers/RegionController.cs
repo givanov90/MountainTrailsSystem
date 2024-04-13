0a6d1772aaeaee6905c471788676660060d3e507
Georgi Ivanov <givanov900@gmail.com>
2024-04-14 00:00:29 +0300

    Admin RegionController implemented

--- MountainTrailsSystem/Areas/Admin/Controllers/RegionController.cs
+++ MountainTrailsSystem/Areas/Admin/Controllers/RegionController.cs
@@ -1,6 +1,43 @@
-﻿namespace MountainTrailsSystem.Areas.Admin.Controllers
+﻿using Microsoft.AspNetCore.Mvc;
+using MountainTrailsSystem.Core.Contracts;
+using MountainTrailsSystem.Core.Models;
+
+namespace MountainTrailsSystem.Areas.Admin.Controllers
 {
     public class RegionController : AdminBaseController
     {
+        private readonly IRegionService regionService;
+        private readonly IMountainService mountainService;
+
+        public RegionController(IRegionService _regionService,
+            IMountainService _mountainService)
+        {
+            regionService = _regionService;
+            mountainService = _mountainService;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Create()
+        {
+            var model = new CreateRegionFormModel();
+
+            model.Mountains = await mountainService.AllMountainsAsync();
+
+            return View(model);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(CreateRegionFormModel model)
+        {
+            if (await regionService.RegionExistsAsync(model.Name))
+            {
+                ModelState.AddModelError(nameof(model.Name), "Region already exists");
+                return View(model);
+            }
+
+            await regionService.CreateRegionAsync(model);
+
+            return RedirectToAction("Index", "Home", new { area = "" });
+        }
     }
 }
