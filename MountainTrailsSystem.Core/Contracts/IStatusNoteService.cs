a45be52d9910e215cf7ba3f5b0bc8f73feb21afc
Georgi Ivanov <givanov900@gmail.com>
2024-04-13 23:59:28 +0300

    StatusNoteService implemented

---
+++ MountainTrailsSystem.Core/Contracts/IStatusNoteService.cs
@@ -0,0 +1,25 @@
+﻿using MountainTrailsSystem.Core.Models;
+
+namespace MountainTrailsSystem.Core.Contracts
+{
+    public interface IStatusNoteService
+    {
+        Task CreateStatusNoteAsync(TrailStatusNoteViewModel model);
+
+        Task<bool> IsStatusNotesNotResolvedAsync();
+
+        Task<int> GetNotResolvedStatusNotesCountAsync();
+
+        Task<IEnumerable<TrailStatusNoteOverviewServiceModel>> AllStatusNotesAsListAsync();
+
+        Task<StatusNoteDetailsViewModel> StatusNoteDetailsByIdAsync(int trailStatusNoteId);
+
+        Task ResolveStatusNoteAsync(int trailStatusNoteId);
+
+        Task<bool> StatusNoteExistsAsync(int trailStatusNoteId);
+
+        Task<bool> IsStatusNoteResolvedAsync(int trailStatusNoteId);
+
+        Task<int> GetTrailByStatusNoteIdAsync(int trailStatusNoteId);
+    }
+}
---
+++ MountainTrailsSystem.Core/Services/StatusNoteService.cs
@@ -0,0 +1,113 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MountainTrailsSystem.Core.Contracts;
+using MountainTrailsSystem.Core.Models;
+using MountainTrailsSystem.Infrastructure.Data;
+using MountainTrailsSystem.Infrastructure.Data.Models;
+
+namespace MountainTrailsSystem.Core.Services
+{
+    public class StatusNoteService : IStatusNoteService
+    {
+        private readonly MountainTrailsSystemDbContext data;
+
+        public StatusNoteService(MountainTrailsSystemDbContext _data)
+        {
+            data = _data;
+        }
+
+        public async Task CreateStatusNoteAsync(TrailStatusNoteViewModel model)
+        {
+            var statusNote = new TrailStatusNote()
+            {
+                Title = model.Title,
+                Description = model.Description,
+                ImageUrl = model.ImageUrl,
+                TrailId = model.TrailId,
+                IsResolved = false
+            };
+
+            await data.TrailStatusNotes.AddAsync(statusNote);
+            await data.SaveChangesAsync();
+        }
+
+        public async Task<bool> IsStatusNotesNotResolvedAsync()
+        {
+            return await data.TrailStatusNotes
+                .AnyAsync(sn => sn.IsResolved == false);
+        }
+
+        public async Task<int> GetNotResolvedStatusNotesCountAsync()
+        {
+            return await data.TrailStatusNotes
+                .Where(sn => sn.IsResolved == false)
+                .CountAsync();
+        }
+
+        public async Task<IEnumerable<TrailStatusNoteOverviewServiceModel>> AllStatusNotesAsListAsync()
+        {
+            return await data.TrailStatusNotes
+                .Where(sn => sn.IsResolved == false)
+                .Select(sn => new TrailStatusNoteOverviewServiceModel()
+                {
+                    TrailStatusNoteId = sn.TrailStatusNoteId,
+                    Title = sn.Title,
+                    ImageUrl = sn.ImageUrl,
+                    Trail = sn.Trail.Name
+                })
+                .ToListAsync();
+        }
+
+        public async Task<StatusNoteDetailsViewModel> StatusNoteDetailsByIdAsync(int trailStatusNoteId)
+        {
+            return await data.TrailStatusNotes
+                .Where(sn => sn.TrailStatusNoteId == trailStatusNoteId)
+                .Select(sn => new StatusNoteDetailsViewModel()
+                {
+                    TrailStatusNoteId = trailStatusNoteId,
+                    Title = sn.Title,
+                    Description = sn.Description,
+                    ImageUrl = sn.ImageUrl,
+                    Trail = sn.Trail.Name,
+                    TrailId = sn.Trail.TrailId
+                })
+                .FirstAsync();
+        }
+
+        public async Task ResolveStatusNoteAsync(int trailStatusNoteId)
+        {
+            var statusNote = await data.TrailStatusNotes
+                .Where(sn => sn.TrailStatusNoteId == trailStatusNoteId)
+                .FirstAsync();
+
+            statusNote.IsResolved = true;
+
+            await data.SaveChangesAsync();
+        }
+
+        public async Task<bool> IsStatusNoteResolvedAsync(int trailStatusNoteId)
+        {
+            var statusNote = await data.TrailStatusNotes
+                .Where(sn => sn.TrailStatusNoteId == trailStatusNoteId)
+                .FirstAsync();
+
+            return statusNote.IsResolved;
+        }
+
+        public async Task<bool> StatusNoteExistsAsync(int trailStatusNoteId)
+        {
+            return await data.TrailStatusNotes
+                .AnyAsync(sn => sn.TrailStatusNoteId == trailStatusNoteId);
+        }
+
+        public async Task<int> GetTrailByStatusNoteIdAsync(int trailStatusNoteId)
+        {
+            var statusNote = await data.TrailStatusNotes
+                .Where(sn => sn.TrailStatusNoteId == trailStatusNoteId)
+                .FirstAsync();
+
+            int trailId = statusNote.TrailId;
+
+            return trailId;
+        }
+    }
+}
