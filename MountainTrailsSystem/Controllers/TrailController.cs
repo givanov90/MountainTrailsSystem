194d10573d638ddda0446f3ef7980a246b2ec22a
Georgi Ivanov <givanov900@gmail.com>
2024-04-14 00:01:36 +0300

    TrailController implemented

--- MountainTrailsSystem/Controllers/TrailController.cs
+++ MountainTrailsSystem/Controllers/TrailController.cs
@@ -1,17 +1,25 @@
 ﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using MountainTrailsSystem.Core.Contracts;
 using MountainTrailsSystem.Core.Models;
+using MountainTrailsSystem.Infrastructure.Data.Models;
 
 namespace MountainTrailsSystem.Controllers
 {
     public class TrailController : BaseController
     {
         private readonly ITrailService trailService;
+        private readonly IRegionService regionService;
+        private readonly UserManager<ApplicationUser> userManager;
 
-        public TrailController(ITrailService _trailService)
+        public TrailController(ITrailService _trailService,
+            IRegionService _regionService,
+            UserManager<ApplicationUser> _userManager)
         {
             trailService = _trailService;
+            regionService = _regionService;
+            userManager = _userManager;
         }
 
         [AllowAnonymous]
@@ -26,6 +34,7 @@ namespace MountainTrailsSystem.Controllers
             return View(model);
         }
 
+        [AllowAnonymous]
         [HttpGet]
         public async Task<IActionResult> Details(int id)
         {
@@ -37,6 +46,108 @@ namespace MountainTrailsSystem.Controllers
             var model = await trailService.TrailDetailsByIdAsync(id);
 
             return View(model);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Save(int id)
+        {
+            if (!await trailService.TrailExistsAsync(id))
+            {
+                return BadRequest();
+            }
+
+            var userId = userManager.GetUserId(User);
+
+            if (userId == null)
+            {
+                return Unauthorized();
+            }
+
+            await trailService.AddTrailToSavedAsync(userId, id);
+
+            return RedirectToAction(nameof(Saved));
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Saved()
+        {
+            var userId = userManager.GetUserId(User);
+
+            var model = await trailService.AllSavedTrailsByUserIdAsync(userId);
+            
+            return View(model);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Unsave(int id)
+        {
+            if (!await trailService.TrailExistsAsync(id))
+            {
+                return BadRequest();
+            }
+
+            var userId = userManager.GetUserId(User);
+
+            if (userId == null)
+            {
+                return Unauthorized();
+            }
+
+            await trailService.RemoveTrailFromSavedAsync(userId, id);
+
+            return RedirectToAction(nameof(Saved));
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Visited()
+        {
+            var userId = userManager.GetUserId(User);
+            var model = await trailService.AllVisitedTrailsByUserIdAsync(userId);
+
+            return View(model);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Visit(int id)
+        {
+            if (!await trailService.TrailExistsAsync(id))
+            {
+                return BadRequest();
+            }
+
+            var userId = userManager.GetUserId(User);
+
+            if (userId == null)
+            {
+                return Unauthorized();
+            }
+
+            await trailService.AddTrailToVisitedAsync(userId, id);
+
+            return RedirectToAction(nameof(Visited));
+        }
+
+        [AllowAnonymous]
+        [HttpGet]
+        public async Task<IActionResult> Find(TrailSearchFormModel model)
+        {
+            model.Regions = await regionService.AllRegionsAsync();
+
+            return View(model);
+        }
+
+        [AllowAnonymous]
+        [HttpGet]
+        public async Task<IActionResult> Search(TrailSearchFormModel model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(model);
+            }
+
+            var searchedTrails = await trailService.FindTrailsByConditionsAsync(model.RegionId, model.DifficultyLevel, model.Duration, model.Elevation);
+
+            return View(searchedTrails);
         }
     }
 }
