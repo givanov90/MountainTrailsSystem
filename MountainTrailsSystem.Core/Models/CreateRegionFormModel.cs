b59d323e031285c2cf9e3021db522e37adf7d798
Georgi Ivanov <givanov900@gmail.com>
2024-04-14 00:04:44 +0300

    Region models created

---
+++ MountainTrailsSystem.Core/Models/CreateRegionFormModel.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+using static MountainTrailsSystem.Infrastructure.Constants.DataConstants;
+using static MountainTrailsSystem.Infrastructure.Constants.MessageConstants;
+
+namespace MountainTrailsSystem.Core.Models
+{
+    public class CreateRegionFormModel
+    {
+        [Required]
+        [StringLength(RegionNameMaximumLength,
+            MinimumLength = RegionNameMinimumLength,
+            ErrorMessage = FieldLengthErrorMessage)]
+        public string Name { get; set; } = string.Empty;
+
+        [Display(Name = "Mountains")]
+        public int MountainId { get; set; }
+        public IEnumerable<MountainServiceModel> Mountains { get; set; } =
+                new List<MountainServiceModel>();
+    }
+}
---
+++ MountainTrailsSystem.Core/Models/RegionServiceModel.cs
@@ -0,0 +1,9 @@
+﻿namespace MountainTrailsSystem.Core.Models
+{
+    public class RegionServiceModel
+    {
+        public int RegionId { get; set; }
+
+        public string Name { get; set; } = string.Empty;
+    }
+}
