298c2d16e55d2045e2ef432fab0f776a5b713141
Georgi Ivanov <givanov900@gmail.com>
2024-04-13 23:58:47 +0300

    MountainService implemented

---
+++ MountainTrailsSystem.Core/Contracts/IMountainService.cs
@@ -0,0 +1,13 @@
+﻿using MountainTrailsSystem.Core.Models;
+
+namespace MountainTrailsSystem.Core.Contracts
+{
+    public interface IMountainService
+    {
+        Task<IEnumerable<MountainServiceModel>> AllMountainsAsync();
+
+        Task CreateMountainAsync(CreateMountainFormModel model);
+
+        Task<bool> MountainExistsAsync(string mountainName);
+    }
+}
---
+++ MountainTrailsSystem.Core/Services/MountainService.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MountainTrailsSystem.Core.Contracts;
+using MountainTrailsSystem.Core.Models;
+using MountainTrailsSystem.Infrastructure.Data;
+using MountainTrailsSystem.Infrastructure.Data.Models;
+using System.Linq;
+
+namespace MountainTrailsSystem.Core.Services
+{
+    public class MountainService : IMountainService
+    {
+        private readonly MountainTrailsSystemDbContext data;
+
+        public MountainService(MountainTrailsSystemDbContext _data)
+        {
+            data = _data;
+        }
+        public async Task<IEnumerable<MountainServiceModel>> AllMountainsAsync()
+        {
+            return await data.Mountains
+                .Select(m => new MountainServiceModel
+                {
+                    MountainId = m.Id,
+                    Name = m.Name
+                })
+                .ToListAsync();
+        }
+
+        public async Task CreateMountainAsync(CreateMountainFormModel model)
+        {
+                var mountain = new Mountain()
+                {
+                     Name = model.Name,
+                };
+
+                    await data.Mountains.AddAsync(mountain);
+                    await data.SaveChangesAsync();
+
+            foreach (var regionId in model.SelectedRegionIds)
+                {
+                    var mountainRegion = new MountainRegion()
+                        {
+                            MountainId = mountain.Id,
+                            RegionId = regionId
+                    };
+
+                    await data.MountainsRegions.AddAsync(mountainRegion);
+                }
+
+                await data.SaveChangesAsync();
+        }
+
+        public async Task<bool> MountainExistsAsync(string mountainName)
+        {
+            return await data.Mountains
+                .AnyAsync(m => m.Name == mountainName);
+        }
+    }
+}
