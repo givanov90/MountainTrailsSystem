a9187b35ff0e31e7b2727d48d8161a6d2b6e29cd
Georgi Ivanov <givanov900@gmail.com>
2024-04-14 00:06:10 +0300

    Trail sort criterias implemented

---
+++ MountainTrailsSystem.Core/Enumerations/TrailSortCriteria.cs
@@ -0,0 +1,14 @@
+﻿namespace MountainTrailsSystem.Core.Enumerations
+{
+    public enum TrailSortCriteria
+    {
+        DifficultyLevelAscending = 0,
+        DifficultyLevelDescending = 1,
+        DurationAscending = 2,
+        DurationDescending = 3,
+        DistanceAscending = 4,
+        DistanceDescending = 5,
+        ElevationGainAscending = 6,
+        ElevationGainDescending = 7,
+    }
+}
