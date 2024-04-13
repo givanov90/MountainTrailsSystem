deccc57916495f0aa9c3c6ffd2fad3bdd7802251
Georgi Ivanov <givanov900@gmail.com>
2024-04-13 23:59:04 +0300

    RegionService implemented

---
+++ MountainTrailsSystem.Core/Contracts/IRegionService.cs
@@ -0,0 +1,13 @@
+﻿using MountainTrailsSystem.Core.Models;
+
+namespace MountainTrailsSystem.Core.Contracts
+{
+    public interface IRegionService
+    {
+        Task CreateRegionAsync(CreateRegionFormModel model);
+
+        Task<bool> RegionExistsAsync(string regionName);
+
+        Task<IEnumerable<RegionServiceModel>> AllRegionsAsync();
+    }
+}
---
+++ MountainTrailsSystem.Core/Services/RegionService.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MountainTrailsSystem.Core.Contracts;
+using MountainTrailsSystem.Core.Models;
+using MountainTrailsSystem.Infrastructure.Data;
+using MountainTrailsSystem.Infrastructure.Data.Models;
+
+namespace MountainTrailsSystem.Core.Services
+{
+    public class RegionService : IRegionService
+    {
+        private readonly MountainTrailsSystemDbContext data;
+
+        public RegionService(MountainTrailsSystemDbContext _data)
+        {
+            data = _data;
+        }
+
+        public async Task<IEnumerable<RegionServiceModel>> AllRegionsAsync()
+        {
+            return await data.Regions
+                .Select(m => new RegionServiceModel
+                {
+                    RegionId = m.RegionId,
+                    Name = m.Name
+                })
+                .ToListAsync();
+        }
+
+        public async Task CreateRegionAsync(CreateRegionFormModel model)
+        {
+            var region = new Region()
+            {
+                Name = model.Name
+            };
+
+            await data.Regions.AddAsync(region);
+            await data.SaveChangesAsync();
+        }
+
+        public async Task<bool> RegionExistsAsync(string regionName)
+        {
+            return await data.Regions
+                .AnyAsync(r => r.Name == regionName);
+        }
+    }
+}
