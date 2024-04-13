094d575a78c2ade0eb8823cc329ec6f3bbbb4dbe
Georgi Ivanov <givanov900@gmail.com>
2024-04-14 00:02:04 +0300

    HomeController modified

--- MountainTrailsSystem/Controllers/HomeController.cs
+++ MountainTrailsSystem/Controllers/HomeController.cs
@@ -1,7 +1,6 @@
 ﻿using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using MountainTrailsSystem.Core.Contracts;
-using MountainTrailsSystem.Models;
 using System.Diagnostics;
 
 namespace MountainTrailsSystem.Controllers
@@ -23,10 +22,21 @@ namespace MountainTrailsSystem.Controllers
             return View(model);
         }
 
+        [AllowAnonymous]
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
+        public IActionResult Error(int statusCode)
         {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+            if (statusCode == 400)
+            {
+                return View("Error400");
+            }
+
+            if (statusCode == 404)
+            {
+                return View("Error404");
+            }
+
+            return View();
         }
     }
 }
