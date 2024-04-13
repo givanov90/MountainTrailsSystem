565f7fff83f342fa1ebd7d4045448ef0e6f4c1d2
Georgi Ivanov <givanov900@gmail.com>
2024-04-14 00:00:16 +0300

    Admin PeakController implemented

--- MountainTrailsSystem/Areas/Admin/Controllers/PeakController.cs
+++ MountainTrailsSystem/Areas/Admin/Controllers/PeakController.cs
@@ -1,6 +1,43 @@
-﻿namespace MountainTrailsSystem.Areas.Admin.Controllers
+﻿using Microsoft.AspNetCore.Mvc;
+using MountainTrailsSystem.Core.Contracts;
+using MountainTrailsSystem.Core.Models;
+
+namespace MountainTrailsSystem.Areas.Admin.Controllers
 {
     public class PeakController : AdminBaseController
     {
+        private readonly IPeakService peakService;
+        private readonly IMountainService mountainService;
+
+        public PeakController(IPeakService _peakService,
+            IMountainService _mountainService)
+        {
+            peakService = _peakService;
+            mountainService = _mountainService;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Create()
+        {
+            var model = new CreatePeakFormModel();
+
+            model.Mountains = await mountainService.AllMountainsAsync();
+
+            return View(model);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(CreatePeakFormModel model, int peakId)
+        {
+            if (await peakService.PeakExistsAsync(peakId))
+            {
+                ModelState.AddModelError(nameof(model.Name), "Peak already exists");
+                return View(model);
+            }
+
+            await peakService.CreatePeakAsync(model);
+
+            return RedirectToAction("Index", "Home", new { area = "" });
+        }
     }
 }
